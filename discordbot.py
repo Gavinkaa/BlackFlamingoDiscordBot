f71b752bd2ddd09a91a2d3b7ca0c599aeb4c1194
Gavinkaa <31143057+Gavinkaa@users.noreply.github.com>
2021-09-11 23:56:06 +0200

    Slash update (#22)
    * Slash commands added for each old command
    
    * Adding decorator.py, custom cooldown usable with slash commands
    
    * Adding cooldown on the slash commands that needed it + handle the related exception
    
    * removing unused function

--- discordbot.py
+++ discordbot.py
@@ -5,11 +5,15 @@ import dateutil.parser
 from dateutil import tz
 import discord
 from discord.ext import commands
+from discord import Client, Intents, Embed
+from discord_slash import SlashCommand, SlashContext
+from discord_slash.utils.manage_commands import create_option
 import json
 import ccxt
 import lending as ld
 import re
 import language_selection as ls
+from decorator import *
 
 with open("config.json") as config_file:
     config = json.load(config_file)
@@ -30,6 +34,11 @@ help_command = commands.DefaultHelpCommand(
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='!',
                    help_command=help_command, intents=intents)
+slash = SlashCommand(bot, sync_commands=True)
+
+
+async def try_slash(ctx):
+    await ctx.send("```Next time you can use the slash command ! (Try it with '/')```")
 
 
 @bot.event
@@ -49,7 +58,6 @@ async def funding(ctx):
         await ctx.send_help(funding)
 
 
-@funding.command(name='bitmex', brief="Display the actual and the predicted funding from bitmex", aliases=['b'])
 async def funding_bitmex(ctx):
     url = "https://www.bitmex.com/api/v1/instrument?symbol=XBTUSD&count=1&reverse=true"
     r = await requests.get(url)
@@ -71,7 +79,20 @@ async def funding_bitmex(ctx):
         ".\n📈 The rate is " + str(round(next_funding * 100, 4)) + "% 📈")
 
 
-@funding.command(name='predicted', brief="Display the predicted funding from several exchanges", aliases=['p'])
+@slash.subcommand(base="funding",
+                  name="bitmex",
+                  description="Display the actual and the predicted funding from bitmex")
+@cooldown(60, 10)  # have to be on the first layer of decorator
+async def _funding_bitmex(ctx):
+    await funding_bitmex(ctx)
+
+
+@funding.command(name='bitmex', brief="Display the actual and the predicted funding from bitmex", aliases=['b'])
+async def _old_funding_bitmex(ctx):
+    await funding_bitmex(ctx)
+    await try_slash(ctx)
+
+
 async def funding_predicted(ctx):
     # First we send all the requests
     url_bitmex = "https://www.bitmex.com/api/v1/instrument?symbol=XBTUSD&count=1&reverse=true"
@@ -124,20 +145,18 @@ async def funding_predicted(ctx):
     )
 
 
-@bot.command(name='fiat', brief="Display the asked fiat rate")
-@commands.cooldown(10, 60, commands.BucketType.default)
-async def fiat(ctx, arg='eurusd'):
-    if(len(arg) == 6 and arg.isalpha()):
-        try:
-            arg = arg.upper()
-            url = "https://api.exchangeratesapi.io/latest?base=" + arg[0:3]
-            r = await requests.get(url)
-            response = r.json()['rates'][arg[3:6]]
-            await ctx.send("```The rate of " + arg[:6] + " is " + str(response) + "```")
-        except:
-            await ctx.send("```Rate not found, please retry```")
-    else:
-        await ctx.send_help(fiat)
+@slash.subcommand(base="funding",
+                  name="predicted",
+                  description="Display the predicted funding from several exchanges")
+@cooldown(60, 10)  # have to be on the first layer of decorator
+async def _funding_predicted(ctx):
+    await funding_predicted(ctx)
+
+
+@funding.command(name='predicted', brief="Display the predicted funding from several exchanges", aliases=['p'])
+async def _old_funding_predicted(ctx):
+    await funding_predicted(ctx)
+    await try_slash(ctx)
 
 
 @bot.group(name='lending', brief="Commands for the KuCoin Crypto Lending USDT section", aliases=['l'])
@@ -147,14 +166,26 @@ async def lending(ctx):
         await ctx.send_help(lending)
 
 
-@lending.command(name='orderbook', brief="Display a graph of the order book", aliases=['ob'])
 async def lending_orderbook(ctx):
     chart_io_bytes = await ld.kucoin_lending_get_orderbook_graph(kucoin)
     chart = discord.File(chart_io_bytes, filename="orderbook.png")
     await ctx.send(file=chart)
 
 
-@lending.command(name='walls', brief="Display the list of walls (up to 10) (minimum 100k)", aliases=['w'])
+@slash.subcommand(base="lending",
+                  name="orderbook",
+                  description="Display a graph of the order book")
+@cooldown(60, 10)  # have to be on the first layer of decorator
+async def _lending_orderbook(ctx):
+    await lending_orderbook(ctx)
+
+
+@lending.command(name='orderbook', brief="Display a graph of the order book", aliases=['ob'])
+async def _old_lending_orderbook(ctx):
+    await lending_orderbook(ctx)
+    await try_slash(ctx)
+
+
 async def lending_walls(ctx, contract_term='all|t7|t14|t28', min_size='100'):
     matched_contract_term = re.search('^t(7|14|28)$', contract_term)
     if matched_contract_term:
@@ -170,7 +201,34 @@ async def lending_walls(ctx, contract_term='all|t7|t14|t28', min_size='100'):
     await ctx.send(msg)
 
 
-@lending.command(name='reach', brief="How much needs to be borrowed to reach a specific rate", aliases=['r'])
+@slash.subcommand(base="lending",
+                  name="walls",
+                  description="Display the list of walls (up to 10) (minimum 100k)",
+                  options=[
+                      create_option(
+                          name="contract_term",
+                          description="contract term (all - t7 - t14 - t28)",
+                          option_type=3,
+                          required=False
+                      ),
+                      create_option(
+                          name="min_size",
+                          description="minimum size (>100k)",
+                          option_type=4,
+                          required=False
+                      )
+                  ])
+@cooldown(60, 10)  # have to be on the first layer of decorator
+async def _lending_walls(ctx, contract_term='all|t7|t14|t28', min_size=100):
+    await lending_walls(ctx, contract_term, str(min_size))
+
+
+@lending.command(name='walls', brief="Display the list of walls (up to 10) (minimum 100k)", aliases=['w'])
+async def _old_lending_walls(ctx, contract_term='all|t7|t14|t28', min_size='100'):
+    await lending_walls(ctx, contract_term, min_size)
+    await try_slash(ctx)
+
+
 async def lending_reach(ctx, arg='2.0'):
     try:
         rate_to_reach = float(arg)
@@ -180,40 +238,81 @@ async def lending_reach(ctx, arg='2.0'):
     await ctx.send(msg)
 
 
+@slash.subcommand(base="lending",
+                  name="reach",
+                  description="How much needs to be borrowed to reach a specific rate",
+                  options=[
+                      create_option(
+                          name="rate",
+                          description="rate to reach",
+                          option_type=3,
+                          required=False
+                      )
+                  ])
+@cooldown(60, 10)  # have to be on the first layer of decorator
+async def _lending_reach(ctx, rate='2.0'):
+    await lending_reach(ctx, rate)
+
+
+@lending.command(name='reach', brief="How much needs to be borrowed to reach a specific rate", aliases=['r'])
+async def _old_lending_reach(ctx, arg='2.0'):
+    await lending_reach(ctx, arg)
+    await try_slash(ctx)
+
+
 @bot.group(name="location", brief="Commands related to the location", aliases=['loc'])
 async def location(ctx):
     if ctx.invoked_subcommand == None:
         await ctx.send_help(location)
 
 
-@location.command(name='town', brief="Use !town YOUR_TOWN to register where you live!")
-async def town(ctx, *, arg=""):
+async def choose_my_town(ctx, arg=""):
     valid_town = await _town_name_valid(ctx, arg)
     if valid_town:
         author_id = str(ctx.author.id)
         try:
-            with open("db.json", "r")as db:
+            with open("users_location.json", "r")as db:
                 users = json.load(db)
                 users.update({author_id: arg.capitalize()})
 
-            with open("db.json", "w")as db:
+            with open("users_location.json", "w")as db:
                 json.dump(users, db)
 
             await ctx.send("{} a été assigné à ton nom !".format(arg.capitalize()))
 
         except FileNotFoundError:
-            with open("db.json", "w") as db:
+            with open("users_location.json", "w") as db:
                 json.dump({author_id: arg.capitalize}, db)
             await ctx.send("Erreur")
 
 
-@location.command(name="whoisat", brief="Enter a town name to see who is nearby!")
-async def whoisat(ctx, *, arg="Paris"):
+@slash.subcommand(base="location",
+                  name="choose-my-town",
+                  description="Register where you live!",
+                  options=[
+                      create_option(
+                          name="town",
+                          description="your town",
+                          option_type=3,
+                          required=True
+                      )
+                  ])
+async def _choose_my_town(ctx, town):
+    await choose_my_town(ctx, town)
+
+
+@location.command(name='choose-my-town', brief="Register where you live!")
+async def _old_choose_my_town(ctx, arg=""):
+    await choose_my_town(ctx, arg)
+    await try_slash(ctx)
+
+
+async def who_is_at(ctx, arg="Paris"):
     valid_town = await _town_name_valid(ctx, arg)
     if valid_town:
         try:
             names_id = []
-            with open("db.json", "r") as db:
+            with open("users_location.json", "r") as db:
                 town = arg.capitalize()
                 db_json = json.load(db)
                 for name_id in db_json.keys():
@@ -223,12 +322,33 @@ async def whoisat(ctx, *, arg="Paris"):
                 if len(names_id) == 0:
                     await ctx.send(f"Personne n'a signalé habiter à {town}")
                 else:
-                    await ctx.send("Les personnes habitant à {} sont les suivantes : \n {}".format(town, "{}\n".join(names_id)))
+                    await ctx.send("Les personnes habitant à {} sont les suivantes : \n{}".format(town, "\n".join(names_id)))
 
         except FileNotFoundError:
-            with open("db.json", "w") as db:
+            with open("users_location.json", "w") as db:
                 json.dump({}, db)
             await ctx.send("Erreur")
+
+
+@slash.subcommand(base="location",
+                  name="who-is-at",
+                  description="Enter a town name to see who is nearby!",
+                  options=[
+                      create_option(
+                          name="town",
+                          description="the town to check",
+                          option_type=3,
+                          required=True
+                      )
+                  ])
+async def _who_is_at(ctx, town):
+    await who_is_at(ctx, town)
+
+
+@location.command(name="who-is-at", brief="Enter a town name to see who is nearby!")
+async def _old_who_is_at(ctx, arg=""):
+    await who_is_at(ctx, arg)
+    await try_slash(ctx)
 
 
 async def _town_name_valid(ctx, town: str) -> bool:
@@ -242,12 +362,11 @@ async def _town_name_valid(ctx, town: str) -> bool:
         return True
 
 
-@location.command(name="where", brief="Use !where @NAME without the #")
-async def where(ctx, *, arg="THISMA"):
+async def where(ctx, arg=""):
     if "<@" in arg and "&" not in arg:
         called_id = arg.strip("<@!>")
         try:
-            with open("db.json", "r") as db:
+            with open("users_location.json", "r") as db:
                 users = json.load(db)
                 if called_id in users:
                     sentence_draw = _random_commenting_sentence()
@@ -256,12 +375,33 @@ async def where(ctx, *, arg="THISMA"):
                     await ctx.send("{} n'a pas donné sa liquidation! heu, sa ville!".format(arg))
 
         except FileNotFoundError:
-            with open("db.json", 'w') as db:
+            with open("users_location.json", 'w') as db:
                 json.dump({}, db)
-            await ctx.send("Erreur")
+            await ctx.send("Error")
 
     else:
         await ctx.send("Merci de tagger le nom de la personne, exemple : !where @THISMA")
+
+
+@slash.subcommand(base="location",
+                  name="where",
+                  description="Check where @user lives",
+                  options=[
+                      create_option(
+                          name="username",
+                          description="the user to check",
+                          option_type=6,
+                          required=True
+                      )
+                  ])
+async def _where(ctx, username: discord.Member):
+    await where(ctx, "<@!" + str(username.id) + ">")
+
+
+@location.command(name="where", brief="Check where @user lives")
+async def _old_where(ctx, arg=""):
+    await where(ctx, arg)
+    await try_slash(ctx)
 
 
 random_sentences = ["Ville des plus gros holders d'EOS", "La ville des adorateurs de $TONE", "aka lamboland", "Lieu préféré de THISMA le boss", "Lieu de pèlerinage TBF",
@@ -275,11 +415,18 @@ def _random_commenting_sentence():
 
 
 @funding.error
-@fiat.error
 @lending.error
 @location.error
 async def on_command_error(ctx, error):
     if isinstance(error, commands.CommandOnCooldown):
+        msg = ':exclamation: To avoid api congestion, this command is on cooldown, please try again in {:.2f}s :exclamation:'.format(
+            error.retry_after)
+        await ctx.reply(msg)
+
+
+@bot.event
+async def on_slash_command_error(ctx, error):
+    if isinstance(error, OnCooldownError):
         msg = ':exclamation: To avoid api congestion, this command is on cooldown, please try again in {:.2f}s :exclamation:'.format(
             error.retry_after)
         await ctx.reply(msg)
