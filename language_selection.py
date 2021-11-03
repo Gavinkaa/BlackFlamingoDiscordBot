af3fac2228efec395a6d7ffa2d3e12d1563f46fb
Ludovic Burnier <ludovic.burnier@epfl.ch>
2021-11-03 08:10:40 +0100

    language selection, better way to get member

--- language_selection.py
+++ language_selection.py
@@ -19,21 +19,21 @@ english_role = None
 
 def get_bf_guid(bot):
     global bf_guild
-    if bf_guild == None:
+    if bf_guild is None:
         bf_guild = get(bot.guilds, id=black_flamingo_discord_id)
     return bf_guild
 
 
 def get_french_role():
     global french_role
-    if french_role == None:
+    if french_role is None:
         french_role = get(bf_guild.roles, id=french_role_id)
     return french_role
 
 
 def get_english_role():
     global english_role
-    if english_role == None:
+    if english_role is None:
         english_role = get(bf_guild.roles, id=english_role_id)
     return english_role
 
@@ -43,19 +43,25 @@ async def add_language_from_reaction(bot, payload):
             payload.channel_id == language_selection_channel_id and \
             payload.message_id == language_selection_message_id:
 
-        if payload.emoji.name == '🇫🇷':
-            bf_guild = get_bf_guid(bot)
-            french_role = get_french_role()
-            member = get(bf_guild.members, id=payload.user_id)
+        if payload.member:
+            member = payload.member
+            print("add from .member")
+        else:
+            guild = await bot.fetch_guild(payload.guild_id)
+            member = get(guild.members, id=payload.user_id)
+            print("add from .get")
 
-            await member.add_roles(french_role)
+        print(member)
+
+        if payload.emoji.name == '🇫🇷':
+            fr_role = get_french_role()
+
+            await member.add_roles(fr_role)
 
         elif payload.emoji.name == '🇬🇧':
-            bf_guild = get_bf_guid(bot)
-            english_role = get_english_role()
-            member = get(bf_guild.members, id=payload.user_id)
+            en_role = get_english_role()
 
-            await member.add_roles(english_role)
+            await member.add_roles(en_role)
 
 
 async def remove_language_from_reaction(bot, payload):
@@ -63,16 +69,22 @@ async def remove_language_from_reaction(bot, payload):
             payload.channel_id == language_selection_channel_id and \
             payload.message_id == language_selection_message_id:
 
-        if payload.emoji.name == '🇫🇷':
-            bf_guild = get_bf_guid(bot)
-            french_role = get_french_role()
-            member = get(bf_guild.members, id=payload.user_id)
+        if payload.member:
+            member = payload.member
+            print("remove from .member")
+        else:
+            guild = await bot.fetch_guild(payload.guild_id)
+            member = get(guild.members, id=payload.user_id)
+            print("remove from .get")
 
-            await member.remove_roles(french_role)
+        print(member)
+        
+        if payload.emoji.name == '🇫🇷':
+            fr_role = get_french_role()
+
+            await member.remove_roles(fr_role)
 
         elif payload.emoji.name == '🇬🇧':
-            bf_guild = get_bf_guid(bot)
-            english_role = get_english_role()
-            member = get(bf_guild.members, id=payload.user_id)
+            en_role = get_english_role()
 
-            await member.remove_roles(english_role)
+            await member.remove_roles(en_role)
